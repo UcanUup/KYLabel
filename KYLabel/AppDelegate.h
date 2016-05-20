3cb729dc0d4035078b05b5bfd7f2436b6ead293d
刘洋洋 <liuyangyang@liuyangyangdemacbook-pro.local>
2016-05-20 16:11:55 +0800

    Initial Commit

---
+++ KYLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KYLabel
+//
+//  Created by 刘洋洋 on 16/5/20.
+//  Copyright © 2016年 刘洋洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KYLabel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KYLabel
+//
+//  Created by 刘洋洋 on 16/5/20.
+//  Copyright © 2016年 刘洋洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
